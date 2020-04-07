08082b5341e02b2e596cc324d67b782d055b39f9
daniel <daniaelcc@gmail.com>
2020-04-07 16:54:05 +0200

    deploy

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const { mongoose } = require('./DB');
-const disDir = __dirname + '/dist/';
 
-const app = express()
+const app = express();
 
 const port = process.env.PORT || 8080;
+
+
 
 // middlewares
 app.use(express.json());
 app.use(cors());
-app.use(express.static(distDir));
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+}
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
