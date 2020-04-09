b06500b669a15134dc6da36e81dab2eeb1441090
daniel <daniaelcc@gmail.com>
2020-04-10 01:26:38 +0200

    testing routes

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use('/employees', express.static('dist/crud-employees'))
 
 // routes
-app.use('/employees', require('./server/routes/employee.routes'));
+app.use('/employees',
+  require('./server/routes/employee.routes'),
+  express.static('dist/crud-employees')
+);
 
 app.listen(port);
