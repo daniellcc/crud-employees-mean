e766b65cce2054d0fa9c74fbc48aadf65beaeb5f
daniel <daniaelcc@gmail.com>
2020-04-10 18:23:36 +0200

    testing

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 const { mongoose } = require('./DB');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-
+// static files
+app.use(express.static(__dirname + '/dist/crud-employees'));
 
 // middlewares
 app.use(express.json());
 app.use(cors());
-app.get('*', (req, res) => res.sendFile(path.resolve('dist/crud-employees/index.html')))
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/dist/crud-employees/index.html'))
+});
 
 app.listen(port);
