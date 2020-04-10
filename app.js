e34abea95eb3dcdc60c73aba9d4553827bf2b88a
daniel <daniaelcc@gmail.com>
2020-04-10 19:50:56 +0200

    testing

--- app.js
+++ app.js
@@ -12,12 +12,9 @@ const port = process.env.PORT || 8080;
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(res.sendFile(path.join(__dirname + '/dist/crud-employees/index.html')))
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
-
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/dist/crud-employees/index.html'))
-});
 
 app.listen(port);
