023975f1b4a47936e9227ba1142dbb83aa62d19a
daniel <daniaelcc@gmail.com>
2020-04-11 22:03:29 +0200

    testing

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 8080;
 // middlewares
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
-	app.use('/*', express.static('dist/crud-employees'));
+	app.use( express.static(path.join(__dirname, 'dist/crud-employees'));
+	app.use('/', express.static(path.join(__dirname, 'dist/crud-employees')));
 }
 
 // routes
