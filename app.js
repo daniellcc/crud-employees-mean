96cb2654f9172b0ec19846ebe03484a9fc9a9d75
daniel <daniaelcc@gmail.com>
2020-04-11 22:53:31 +0200

    testing

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+		res.sendFile(__dirname + '/dist/view/crud-employees/index.html');
+	});
+
 // middlewares
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
-	app.get('/', (req, res) => {
-		res.sendFile(__dirname + '/dist/view/crud-employees/index.html');
-	});
-	app.use('/static', express.static(path.join(__dirname, 'dist/static')));
+	app.use(express.static(path.join(__dirname, 'dist/static')));
 }
 
 // routes
