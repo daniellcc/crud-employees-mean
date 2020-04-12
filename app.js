19227f3f60a9662a3d17189de13c04381d6bcf93
daniel <daniaelcc@gmail.com>
2020-04-12 02:16:12 +0200

    testing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(__dirname + '/dist'));
+	app.use(express.static('dist'));
 }
 
 // routes
