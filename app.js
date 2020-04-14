c3c2d220b0563a42b7fceba9168296779cba5ae2
daniel <daniaelcc@gmail.com>
2020-04-13 23:44:56 -0430

    testing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { mongoose } = require('./DB');
 
 const app = express();
@@ -9,7 +10,11 @@ const port = process.env.PORT || 8080;
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
