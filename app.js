5bc72a8b54f051ca51db6eb988894dcaf219a5e3
daniel <daniaelcc@gmail.com>
2020-04-18 03:28:14 -0430

    testing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public'));
+app.use('*', express.static(path.join(__dirname, 'public')))
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
