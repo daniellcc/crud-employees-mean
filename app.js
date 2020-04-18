76d4598e7bc8a5060a5efc60a8882612377f8304
daniel <daniaelcc@gmail.com>
2020-04-18 00:09:56 -0430

    testing

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
+
+// not found 404
+app.use((req, res, next) => {
+	next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	console.error(err.message);
+	if(!err.statusCode) err.statusCode = 500;
+	res.status(err.statusCode.send(err.message));
+})
 
 app.listen(port);
