c76396e804eff78c63caf7c923cf422ae115b8f2
daniel <daniaelcc@gmail.com>
2020-04-18 03:06:49 -0430

    testing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { mongoose } = require('./DB');
 const app = express();
 
 const port = process.env.PORT || 8080;
-
+app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('*', express.static(path.join(__dirname, 'public')));
