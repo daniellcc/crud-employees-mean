00974a4864da1cd0506724c144d1db055574f673
daniel <daniaelcc@gmail.com>
2020-04-21 03:18:46 -0430

    testing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req,res) => {
+app.get('/employees', (req,res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
 })
 
