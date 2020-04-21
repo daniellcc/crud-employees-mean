05e49e4f7e8610e4ad2d1ab2cdd3fd8547a5f3b1
daniel <daniaelcc@gmail.com>
2020-04-21 01:01:35 -0430

    testing

--- app.js
+++ app.js
@@ -11,14 +11,11 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('*', express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 // not found 404
 app.use((req, res, next) => {
