723c03f6de6112e9de652a6663ad46983ea4da1d
daniel <daniaelcc@gmail.com>
2020-04-21 01:27:34 -0430

    testing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 8080;
 // middlewares
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
