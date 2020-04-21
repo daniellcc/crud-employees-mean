69e360404c9dffca58ac5b3fc9b7c2a07e681452
daniel <daniaelcc@gmail.com>
2020-04-21 17:30:41 -0430

    bundles

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 // middlewares
 app.use(cors());
 
-app.use('*', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
