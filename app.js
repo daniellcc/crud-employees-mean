e34fde057f8f0b58d4611d9792a1e545d7192eb1
daniel <daniaelcc@gmail.com>
2020-04-21 20:18:21 -0430

    testin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const serveStatic = require('serve-static')
 const { mongoose } = require('./DB');
 
 const app = express();
@@ -11,13 +12,9 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, 'public')));
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 app.listen(port);
