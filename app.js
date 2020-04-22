d645913a4e6ed794471a58acab712b5c6da124ba
daniel <daniaelcc@gmail.com>
2020-04-21 19:31:12 -0430

    testing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/employees', require('./server/routes/employee.routes'));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'view/index.html'));
+	res.send('hey');
 });
 
 app.listen(port);
