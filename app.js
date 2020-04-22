2f5fb6a8467017a2df5d74fa3938ddc034104a28
daniel <daniaelcc@gmail.com>
2020-04-21 20:20:58 -0430

    testin

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-// middlewares
-app.use(express.json());
-app.use(cors());
 
 app.use(serveStatic(path.join(__dirname, 'public')));
 
