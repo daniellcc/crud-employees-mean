8f2373240d0aa083188707a70e0db0bef52f9759
daniel <daniaelcc@gmail.com>
2020-04-23 20:13:27 -0430

    more testing

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req,res) => {
+app.get('/', (req,res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
