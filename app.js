4e1f658d1792223e33298b7adba3dc5bb6f4c71a
daniel <daniaelcc@gmail.com>
2020-04-23 20:03:27 -0430

    more testing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 
 app.use(compression());
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
