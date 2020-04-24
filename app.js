be9462f9daabfb0066089f70534afc65a95b2470
daniel <daniaelcc@gmail.com>
2020-04-23 19:58:00 -0430

    more testing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const compression = require('compression');
 const serveStatic = require('serve-static');
-const compression = require('compresison');
 const { mongoose } = require('./DB');
 
 const app = express();
