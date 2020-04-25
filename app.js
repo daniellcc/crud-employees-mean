b782d1907774f062d2762b9e9907207e76f0d411
daniel <daniaelcc@gmail.com>
2020-04-25 00:44:56 -0430

    testing

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ const port = process.env.PORT || 8080;
 app.use(compression());
 app.use(cors());
 
-app.use('/public', serveStatic(path.join(__dirname, 'public')))
-
-app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+app.use(serveStatic(path.join(__dirname, 'public')))
 
 // routes
 app.use('/employees', require('./server/routes/employee.routes'));
