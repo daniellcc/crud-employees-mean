2c11b65e5b006e4eb1cdd8c1e1b5d6c3613bb291
daniel <daniaelcc@gmail.com>
2020-04-25 00:33:40 -0430

    testing

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const { mongoose } = require('./DB');
 
 const app = express();
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.use(compression());
 app.use(cors());
 
-app.use(serveStatic(path.join(__dirname, 'public')))
+app.use('/public', serveStatic(path.join(__dirname, 'public')))
 
 app.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
