ee9af5b5f8048cb7a5c30fa0340fbbb9ff2e54ba
daniel <daniaelcc@gmail.com>
2020-04-25 02:35:26 -0430

    testing

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(compression());
-
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/public'));
 
