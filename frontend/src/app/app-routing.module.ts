81440e4e551d5e947b16010c1bf8b9cd361b0262
daniel <daniaelcc@gmail.com>
2020-04-25 00:25:28 -0430

    testing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,19 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DatabaseCardComponent } from './components/database-card/database-card.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'employees',
-    children: [
-      { path: '', component: DatabaseCardComponent },
-      { path: 'emp/:id', component: EmployeeComponent }
-    ]
-  }, 
-  { path: '', redirectTo:'/employees', pathMatch: 'full'}
+  { path: '', redirectTo:'/employees', pathMatch: 'full'},
+  { path: 'employees', component: DatabaseCardComponent },
+  { path: 'emp/:id', component: EmployeeComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
