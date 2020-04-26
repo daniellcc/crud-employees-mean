fae31a38357db722620b267407618d863678b38e
daniel <daniaelcc@gmail.com>
2020-04-25 22:20:25 -0430

    testin redirects

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ const port = process.argv[2]; // get port from command line argument
 const root = __dirname + '/public';
 
 // public
+app.use(express.static(root));
 
 // history fallback
-app.use(fallback(__dirname + '/public/index.html'))
+app.use(fallback(__dirname + '/index.html'))
 
 app.use(cors());
 
+// routes
+app.use('/employees', require('./server/routes/employee.routes'));
 
 
 app.listen(port);
