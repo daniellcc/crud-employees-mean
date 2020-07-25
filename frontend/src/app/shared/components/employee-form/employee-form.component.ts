62cddc52870bf4682e6261b959d3b7d77b265c70
daniellcc <daniaelcc@gmail.com>
2020-07-25 01:57:02 -0430

    unsubscribtion added

--- frontend/src/app/shared/components/employee-form/employee-form.component.ts
+++ frontend/src/app/shared/components/employee-form/employee-form.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, OnDestroy, Inject} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Employee } from 'src/app/models/employee';
 import { Router } from '@angular/router';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
  
 import { EmployeesService } from 'src/app/services/employees.service';
 
-import { Subscription } from 'rxjs';
+import {Observable } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -13,19 +13,16 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.css']
 })
-export class EmployeeFormComponent implements OnInit, OnDestroy {
-  addEmployee: boolean;
-
-  type: string;
-
+export class EmployeeFormComponent implements OnInit {
   employee: Employee = new Employee();
+  addEmployee: boolean;
+  type: string;
 
   jobs: string[] = ['CTO', 'General Manager', 'HR Manager', 'Marketing Director', 'Marketer', 'Lead Developer', 'Developer'];
 
   constructor(private router: Router,
               private employeesService: EmployeesService,
               private toastr: ToastrService,
-              private dialogRef: MatDialogRef<EmployeeFormComponent>,
               @Inject(MAT_DIALOG_DATA) data) {
     
     this.type = data.type;
@@ -33,43 +30,61 @@ export class EmployeeFormComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.type == 'edit'
-      ? this.addEmployee = false
-      : this.addEmployee = true;
+    this.evalFormType();
   }
 
-  ngOnDestroy(): void {
+  evalFormType() {
+    if(this.type == 'edit') {
+      this.addEmployee = false;
+      this.getEmployee();
+    }
+    else this.addEmployee = true;
   }
 
-  getEmp(): Subscription {
-    return this.employeesService.getEmployee(this.employee._id).subscribe(emp => {
-      this.employee = emp;
+  getEmployee(): void {
+    const getEmp = new Observable((sub) => {
+      sub.next(this.employeesService.getEmployee(this.employee._id));
+      sub.complete();
+    });
+
+    getEmp.subscribe({
+      next(data: Employee) { this.employee = data },
+      error(err) { this.toastr.error(err.message, 'Error on Getting', {timeOut: 5000}) }
     });
   }
 
-  newEmployee(): Subscription {
-    if(this.employee.name && this.employee.job && this.employee.email) {
-      return this.employeesService.addEmployee(this.employee)
-        .subscribe(
-          () => this.employee = new Employee(),
-          (error: Error) => this.toastr.error(error.message, 'Error on Adding', {timeOut: 5000}),
-          () => {
-            this.router.navigate(['/']);
-            this.toastr.success('Employee added', '',{timeOut: 2000});
-          }
-        );
-    }
+  newEmployee(): void {
+    const newEmp = new Observable((sub) => {
+      sub.next(() => {
+        if(this.employee.name && this.employee.job && this.employee.email)
+          this.employeesService.addEmployee(this.employee);
+      });
+      sub.complete();
+    })
+
+    newEmp.subscribe({
+      next() { this.employee = new Employee() },
+      error(err) { this.toastr.error(err.message, 'Error on Adding', {timeOut: 5000}) },
+      complete() {
+        this.router.navigate(['/']);
+        this.toastr.success('Employee added', '',{timeOut: 2000});
+      }
+    });
   }
 
-  editEmployee(): Subscription {
-    return this.employeesService.editEmployee(this.employee)
-      .subscribe(
-        () => true,
-        (error: Error) => this.toastr.error(error.message, 'Error on Editing', {timeOut: 5000}),
-        () => {
-          this.router.navigate(['/']);
-          this.toastr.success('Employee updated', '', {timeOut: 2000});
-        }
-      );
+  editEmployee(): void {
+    const editEmp = new Observable(sub => {
+      sub.next(this.employeesService.editEmployee(this.employee));
+      sub.complete();
+    });
+
+    editEmp.subscribe({
+      next() {},
+      error(error) { this.toastr.error(error.message, 'Error on Editing', {timeOut: 5000}) },
+      complete() {
+        this.router.navigate(['/']);
+        this.toastr.success('Employee updated', '', {timeOut: 2000});
+      }
+    });
   }
 }
